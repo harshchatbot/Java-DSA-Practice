d947425b4f8ed745b93d3976463c6db6cf65cfe2
Harsh Veer <40229369+harshchatbot@users.noreply.github.com>
2026-02-26 19:42:33 +0530

    linkedlist

---
+++ src/DSA/datastructures/linkedlist/LinkedList.java
@@ -0,0 +1,50 @@
+package DSA.datastructures.linkedlist;
+
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        //constructor for inner class i.e this node class
+        Node(int value){
+            this.value = value;
+        }
+
+        
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    public void printList(){
+        Node temp = head;
+        while(temp != null){
+            System.out.println(temp.value);
+            temp = temp.next;
+
+        }
+        
+    }
+
+    public void getHead(){
+        System.out.println("Head: "+ head.value);
+    }
+
+    public void getTail(){
+        System.out.println("Tail: "+ tail.value);
+    }
+
+    public void getLength(){
+        System.out.println("Length: "+ length);
+    }
+    
+}
---
+++ src/DSA/datastructures/linkedlist/Main.java
@@ -0,0 +1,18 @@
+package DSA.datastructures.linkedlist;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        LinkedList myLinkedList = new LinkedList(4);
+
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+
+        myLinkedList.printList();
+
+    }
+
+    
+}
