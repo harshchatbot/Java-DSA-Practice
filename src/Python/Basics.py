34bf48e8c8a66e31dac23effffb04d6988471c37
Harsh Veer <40229369+harshchatbot@users.noreply.github.com>
2026-09-03 01:24:25 +0530

    booleans

--- src/Python/Basics.py
+++ src/Python/Basics.py
@@ -47,3 +47,43 @@ print(float(10))
 print(int(3.33))
 # They can even be called on strings!
 print(int('807') + 1)
+
+
+
+
+"""Functions and Getting Help"""
+
+#help(round)
+#help(print)
+
+"""Defining functions"""
+
+def addition(a,b):
+    #Python isn't smart enough to read my code and turn it into a nice English description. 
+    # However, when I write a function, I can provide a description in what's called the docstring.
+    """This functipon takes two numbers and adds them"""  #docstring
+    return a + b
+
+print("addition : ", addition(2,3))
+
+#help(addition)
+
+
+
+#Note: python has None instead of null ok
+
+print(1, 2, 3, sep=' < ') #separater 
+
+#But if we don't specify a value, sep is treated as having a default value of ' ' (a single space).
+print(1, 2, 3)
+
+
+
+#Functions that don't return¶
+#Functions Applied to Functions¶
+
+
+
+
+"""Booleans and Conditionals"""
+
