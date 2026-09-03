8bb248c56e6769911fd06757008ecdaf9ece8d0a
Harsh Veer <40229369+harshchatbot@users.noreply.github.com>
2026-09-03 10:16:29 +0530

    arithematic

--- src/Python/Basics.py
+++ src/Python/Basics.py
@@ -25,6 +25,20 @@ print(5 // 2)
 
 
 
+"""
+Operator	Name	Description
+a + b	Addition	Sum of a and b
+a - b	Subtraction	Difference of a and b
+a * b	Multiplication	Product of a and b
+a / b	True division	Quotient of a and b
+a // b	Floor division	Quotient of a and b, removing fractional parts
+a % b	Modulus	Integer remainder after division of a by b
+a ** b	Exponentiation	a raised to the power of b
+-a	Negation	The negative of a
+"""
+
+
+
 #Order of operations
 #The arithmetic we learned in primary school has conventions about the order 
 # in which operations are evaluated. Some remember these by a mnemonic such as 
@@ -87,3 +101,30 @@ print(1, 2, 3)
 
 """Booleans and Conditionals"""
 
+x = True
+print(x)
+print(type(x))
+
+"""
+Comparison Operations
+Operation	Description		Operation	Description
+a == b	a equal to b		a != b	a not equal to b
+a < b	a less than b		a > b	a greater than b
+a <= b	a less than or equal to b		a >= b	a greater than or equal to b
+"""
+
+print(3.0 == 3) #True
+
+print('3' == 3) #False, because one is a string and the other is an integer
+
+#Remember to use == instead of = when making comparisons. 
+#If you write n == 2 you are asking about the value of n. 
+# When you write n = 2 you are changing the value of n.
+
+
+#Q: Guess its value?
+print("Guess ? ",True or True and False)
+
+#Note: 'and' is evaluated before 'or'
+#so for better visibility use parenthesis
+print("Guess now ? ",True or (True and False))
